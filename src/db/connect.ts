9c6679cbdfca3f3b65cce530306b68dffcb8987c
mohanadft <mohanadfteha@gmail.com>
2023-06-10 22:29:20 +0300

    Setup database connection and schema

---
+++ src/db/connect.ts
@@ -0,0 +1,14 @@
+import { Sequelize } from "sequelize"
+
+const conn = new Sequelize("nodemysql", "root", "", {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+conn.authenticate().then(() => {
+  console.log("Successfully connected to the database");
+}).catch(e => {
+  console.log("Error connecting to a database", e);
+})
+
+export default conn
---
+++ src/db/models/Book.ts
@@ -0,0 +1,43 @@
+import { DataTypes } from 'sequelize';
+import conn from '../connect';
+
+const Book = conn.define('Book', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      max: 20,
+    }
+  },
+  author: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      max: 20
+    }
+  },
+  isbn: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      min: 10,
+      max: 30
+    }
+  }
+}, {
+  freezeTableName: true,
+  timestamps: false
+});
+
+Book.sync().then(() => {
+  console.log("Table successfully synced");
+}).catch((err) => {
+  console.log("Error syncing the table");
+})
+
+export default Book
