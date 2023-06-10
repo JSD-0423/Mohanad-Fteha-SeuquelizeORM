26aad3513e4473e09bfc5dfee4b7532c5c033131
mohanadft <mohanadfteha@gmail.com>
2023-06-10 22:30:23 +0300

    Handle all features in the main file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("s")
+import express, { Application } from "express"
+import bookRouter from "./routes/book.route";
+
+const app: Application = express();
+app.use('/books', bookRouter);
+
+app.listen(3000, () => console.log("Successfully connected to the server"))
